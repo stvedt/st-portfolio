94b081819e5e26429ac260ae55559a8f146848a9
Stephen Tvedt <stephen@stephentvedt.com>
2017-09-17 10:48:10 -0400

    add drawer for mobile

--- src/components/nav_bar.js
+++ src/components/nav_bar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
 import FontIcon from 'material-ui/FontIcon';
 import '../css/mui-icon-custom.css';
 import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
@@ -9,29 +11,51 @@ const styles = {
   }
 }
 
-const NavBar = () => {
-  return (
-    <Toolbar>
-      <ToolbarTitle text="Stephen Tvedt" />
-      <ToolbarGroup>
-        <RaisedButton
-          href="https://github.com/stvedt"
-          target="_blank"
-          label="Github Link"
-          style={styles.button}
-          icon={<FontIcon className="muidocs-icon-custom-github" />}
-        />
-        <RaisedButton
-          href="https://github.com/stvedt"
-          target="_blank"
-          label="Resume/CV"
-          download
-          style={styles.button}
-          icon={<FontIcon className="material-icons">file_download</FontIcon>}
-        />
-      </ToolbarGroup>
-    </Toolbar>
-  )
+class NavBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+
+  render() {
+    return (
+      <div>
+        <Toolbar>
+          <div className="hidden-md-up">
+            <RaisedButton
+              label="Toggle Drawer"
+              onClick={this.handleToggle}
+            />
+          </div>
+          <ToolbarTitle text="Stephen Tvedt" />
+          <ToolbarGroup className="hidden-md-down">
+            <RaisedButton
+              href="https://github.com/stvedt"
+              target="_blank"
+              label="Github"
+              style={styles.button}
+              icon={<FontIcon className="muidocs-icon-custom-github" />}
+            />
+            <RaisedButton
+              href="https://github.com/stvedt"
+              target="_blank"
+              label="Resume"
+              download
+              style={styles.button}
+              icon={<FontIcon className="material-icons">file_download</FontIcon>}
+            />
+          </ToolbarGroup>
+        </Toolbar>
+        <Drawer open={this.state.open}>
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
+      </div>
+    )
+  }
 }
 
 export default NavBar;
