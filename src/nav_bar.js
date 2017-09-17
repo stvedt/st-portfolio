55c4e38f6f674c278a6e0680e5d01e4cef8ccea2
Stephen Tvedt <stephen@stephentvedt.com>
2017-09-17 10:23:03 -0400

    add structure of components for work and add frameworks materialUI and bootstrap for grid

---
+++ src/nav_bar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
+import './mui-icon-custom.css';
+import {Toolbar, ToolbarGroup, ToolbarTitle} from 'material-ui/Toolbar';
+const styles = {
+  button: {
+    margin: 12,
+  }
+}
+
+const NavBar = () => {
+  return (
+    <Toolbar>
+      <ToolbarTitle text="Stephen Tvedt" />
+      <ToolbarGroup>
+        <RaisedButton
+          href="https://github.com/stvedt"
+          target="_blank"
+          label="Github Link"
+          style={styles.button}
+          icon={<FontIcon className="muidocs-icon-custom-github" />}
+        />
+        <RaisedButton
+          href="https://github.com/stvedt"
+          target="_blank"
+          label="Resume/CV"
+          download
+          style={styles.button}
+          icon={<FontIcon className="material-icons">file_download</FontIcon>}
+        />
+      </ToolbarGroup>
+    </Toolbar>
+  )
+}
+
+export default NavBar;
