889e5e83985d26571e7ecfce62a747b8b765d5d9
Stephen Tvedt <stephen@stephentvedt.com>
2024-01-05 15:58:40 -0500

    unregister service worker cache too aggressive

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import App from "./App";
+// import registerServiceWorker from "./registerServiceWorker";
+import { unregister } from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById("root"));
+// registerServiceWorker();
+unregister();
